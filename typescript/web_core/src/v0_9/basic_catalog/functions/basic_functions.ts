44205ea119f883e11eeb053b9311be1217b0d9eb
Greg Spencer <gspencer@google.com>
2026-08-17 17:18:01 -0700

    fix(web_core): update IndexApi import path from v1_0/functions/system_functions

--- typescript/web_core/src/v0_9/basic_catalog/functions/basic_functions.ts
+++ typescript/web_core/src/v0_9/basic_catalog/functions/basic_functions.ts
@@ -45,8 +45,8 @@ import {
   FormatDateApi,
   PluralizeApi,
   OpenUrlApi,
-  IndexApi,
 } from './basic_functions_api.js';
+import {IndexApi} from '../../../v1_0/functions/system_functions.js';
 import {A2uiExpressionError} from '../../../errors.js';
 
 // Arithmetic
